c62f8b7f0a57244adbf7f96330d3cc3b298fbe0f
calle fransson <calle-fransson@hotmail.com>
2023-10-09 11:28:28 +0200

    Add project files.

---
+++ Phone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneInfo
+{
+    internal class Phone
+    {
+        public string Company;
+        public string Model;
+        public string ReleaseDay;
+
+        // Place for your 3 constructors
+        public Phone()
+        {
+            Company = "unknown";
+            Model = "unknown";
+            ReleaseDay = "unknown";
+        }
+        public Phone(string company,string model)
+        {
+            Company = company;
+            Model = model;
+            ReleaseDay = "unknown";
+        }
+        public Phone(string company,string model,string releaseDay)
+        {
+            Company = company;
+            Model = model;
+            ReleaseDay = releaseDay;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine("It is {0} created by {1}. It was released {2}.", Model, Company, ReleaseDay);
+            //Console.WriteLine($"It is {Model} created by {Company}. It was released {ReleaseDay}.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PhoneInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone iPhone = new Phone();
+            iPhone.Introduce();
+            Phone samsung = new Phone("Samsung", "Galaxy Z flip5");
+            samsung.Introduce();
+            Phone nokia = new Phone("Nokia", "nokia 3310", "1 September 2000");
+            nokia.Introduce();
+        }
+    }
+}
